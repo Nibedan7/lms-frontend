8a5f16748d53ae2b43cd250f1f168079450bdce5
NIBEDAN PATTANAIK <119337633+Nibedan7@users.noreply.github.com>
2023-10-23 19:12:31 +0530

    Added 404 not found page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,42 @@
+import apj from "../Assets/Images/apj.png";
+import billGates from "../Assets/Images/billGates.png";
+import einstein from "../Assets/Images/einstein.png";
+import nelsonMandela from "../Assets/Images/nelsonMandela.png";
+import steveJobs from "../Assets/Images/steveJobs.png";
+
+export const celebrities = [
+    {
+      title: "Nelson Mandela",
+      description: "You can never have an impact on society if you have not changed yourself.",
+      image: nelsonMandela,
+      slideNumber: 1
+    },
+
+    {
+        title: "APJ Abdul Kalam",
+        description: "Failure will never overtake me if my determination to succced is strong enough.",
+        image: apj,
+        slideNumber: 2
+      },
+
+      {
+        title: "Albert Einstein",
+        description: "A person who never made a mistake tried anything new.",
+        image: einstein,
+        slideNumber: 3
+      },
+
+      {
+        title: "Steve Jobs",
+        description: "we don't get a chance to do that many things, and every one should be really excellent.",
+        image: steveJobs,
+        slideNumber: 4
+      },
+
+      {
+        title: "Bill Gates",
+        description: "Success is a lousy teacher. It seduses smart people into thinking they can't lose.",
+        image: billGates,
+        slideNumber: 5
+      },
+]
